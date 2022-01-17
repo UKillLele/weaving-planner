9752d7075fe74be7ea1e90acd249d86b93fbc5d4
ImprovingAnna <anna.mayer@improving.com>
2022-01-17 17:10:35 -0600

    fix api json structure

--- api/getPatterns/index.ts
+++ api/getPatterns/index.ts
@@ -3,10 +3,9 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: "you did it!"
-    };
+    context.res.json({
+        text: "you did it!"
+    })
 
 };
 
