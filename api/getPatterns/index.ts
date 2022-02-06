c3db89d6433f11c9dd03d749ffe404c321218262
UKillLele <mayerannam@gmail.com>
2022-02-05 20:41:08 -0600

    order patterns alphabetically

--- api/getPatterns/index.ts
+++ api/getPatterns/index.ts
@@ -4,9 +4,15 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('HTTP trigger function processed a get all request.');
 
     try {
-        context.res.status(200).json({ success: true, data: context.bindings.patternData});
+        context.res.status(200).json({ 
+          success: true, 
+          data: context.bindings.patternData
+            .sort((a: any, b: any) => {
+              a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1 
+            })
+        });
       } catch (error) {
-        context.res.status(500).json({ success: true, error: error});
+        context.res.status(500).json({ success: false, error: error});
     }
 
 };
